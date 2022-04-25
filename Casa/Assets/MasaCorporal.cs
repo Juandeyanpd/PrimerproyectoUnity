750998ed78ddbe259bf738745035329f3c069d01
unknown <SENA@MEDAPRCCSFSD350.FormacionSena.RED>
2022-04-25 15:12:14 -0500

    Nuevo Script 25/04/2022

---
+++ Casa/Assets/MasaCorporal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasaCorporal : MonoBehaviour
+{
+    public string nombreDeUsuario;
+    public float altura;
+    public float kilogramos;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        Debug.Log("Mi nombre es: " + nombreDeUsuario);
+        Debug.Log("Mi altura es: " + altura);
+        Debug.Log("Mi peso es: " + kilogramos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
